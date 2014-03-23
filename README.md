fcd2e650029656e921b608eba4d005ee8cee9b74
Tako Marks <tako.marks@gmail.com>
2014-03-23 18:36:37 +0100

    Eerste commit, met schrale readme + gitignore

---
+++ README.md
@@ -0,0 +1,4 @@
+== Operation Constipated Koala
+De bedoeling is dat dit een (leden)administratie en informatie systeem wordt voor komende besturen van Studievereniging Sticky.
+
+Wordt vervolgd...
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ConstipatedKoala::Application.config.secret_key_base = '771f7ea10948a3912f6ae8817c48c652b9ba697f1f4c0c4a2644b4de3b39cb0f7d8e29de7115efed966037a3309e4c3f597cc6eb0295425f4e05da4e864213b4'
