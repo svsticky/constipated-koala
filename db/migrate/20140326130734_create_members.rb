aedf621f3f4168f8e1759424b11478d30be45cdd
Tako Marks <tako.marks@gmail.com>
2014-03-26 14:13:02 +0100

    Member migration toegevoegd

---
+++ db/migrate/20140326130734_create_members.rb
@@ -0,0 +1,22 @@
+class CreateMembers < ActiveRecord::Migration
+  def change
+    create_table :members do |t|
+      t.string :first_name
+      t.string :infix
+      t.string :last_name
+      t.string :address
+      t.string :house_number
+      t.string :postal_code
+      t.string :city
+      t.string :phone_number
+      t.string :email
+      t.string :gender
+      t.integer :student_id
+      t.date :birth_date
+      t.date :join_date
+      t.text :comments
+
+      t.timestamps
+    end
+  end
+end
