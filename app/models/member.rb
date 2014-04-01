094468e6ec002c3679d8c755e7610a1487ac4b81
Tako Marks <tako.marks@gmail.com>
2014-04-01 14:35:53 +0200

    Eerste opzet input validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,16 @@
 class Member < ActiveRecord::Base
+  validates :first_name, presence: true
+  #validates :infix
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :house_number, presence: true
+  validates :postal_code, presence: true
+  validates :city, presence: true
+  validates :phone_number, presence: true
+  validates :email, presence: true
+  validates :gender, presence: true
+  validates :student_id, presence: true
+  validates :birth_date, presence: true
+  validates :join_date, presence: true
+  #validates :comments
 end
