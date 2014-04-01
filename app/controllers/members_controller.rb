74092b5b30b7d2278541ec3c885229ab56d0f8a6
Tako Marks <tako.marks@gmail.com>
2014-04-01 16:25:36 +0200

    Member edit werkt met karige validation

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -10,4 +10,32 @@ class MembersController < ApplicationController
   def edit
     @member = Member.find(params[:id])
   end
+
+  def update
+    @member = Member.find(params[:id])
+
+    if @member.update(admin_post_params)
+      redirect_to @member
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def admin_post_params
+    params[:member].permit(:first_name,
+                                   :infix,
+                                   :last_name,
+                                   :address,
+                                   :house_number,
+                                   :postal_code,
+                                   :city,
+                                   :phone_number,
+                                   :email,
+                                   :gender,
+                                   :student_id,
+                                   :birth_date,
+                                   :join_date,
+                                   :comments)
+  end
 end
