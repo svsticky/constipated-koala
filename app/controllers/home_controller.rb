89ae7719a06be48895ac1dfeacf65316374d4aab
Martijn Casteel <martijn.casteel@gmail.com>
2014-07-10 15:41:20 +0200

    Home page anders opgezet, en migration gerenamed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @members = Education.group('member_id').where('end_date IS NULL').length
+    @activities = 0
+    @sales = 0
+    @unpayed = '0,00'
   end
 end
