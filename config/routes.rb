85f6f418a8e2e1e3f7493181d3752b5a49bd0d57
Martijn Casteel <martijn.casteel@gmail.com>
2014-08-09 15:29:59 +0200

    Routes opgedeeld in twee subdomeinen; koala en intro.

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,36 @@
 ConstipatedKoala::Application.routes.draw do
-  # You can have the root of your site routed with "root"
-  root 'home#index'
+  constraints :subdomain => 'koala' do
+    # You can have the root of your site routed with "root"
+    root 'home#index'
    
-  # Home controller
-  get 'home' => 'home#index'
+    # Home controller
+    get 'home' => 'home#index'
   
-  # Devise routes
-  devise_for :admins, controllers: 
+    # Devise routes
+    devise_for :admins, controllers: 
     {
       registrations: "admin_devise/registrations",
       unlocks: "admin_devise/unlocks",
       passwords: "admin_devise/passwords"
     }
     
-    
-  # Resource pages
-  resources :members, :activities
-   
-  # Public pages for member registration
-  get 'public' => 'public#index'
-  post 'public' => 'public#create'
+    # Resource pages
+    resources :members, :activities
   
-  # Participants routes for JSON calls
-  get 'participants/list' => 'participants#list'
-  get 'participants' => 'participants#find'
-  post 'participants' => 'participants#create'
-  patch 'participants' => 'participants#update'
-  delete 'participants' => 'participants#destroy'
+    # Participants routes for JSON calls
+    get 'participants/list' => 'participants#list'
+    get 'participants' => 'participants#find'
+    post 'participants' => 'participants#create'
+    patch 'participants' => 'participants#update'
+    delete 'participants' => 'participants#destroy'
+  end
 
- end
+  constraints :subdomain => 'abc' do
+    match '/', to: 'public#index', via: :get, as: 'public'
+    match '/', to: 'public#create', via: :post
+
+    # Public pages for member registration
+#    get 'public' => 'public#index'
+#    post 'public' => 'public#create'
+  end
+end
