a9f65ea177bec1959542ab28a897f1dfa83472c8
Martijn Casteel <martijn.casteel@gmail.com>
2014-09-01 16:40:27 +0200

    Education.rb opgeruimd

--- app/models/education.rb
+++ app/models/education.rb
@@ -4,38 +4,4 @@ class Education < ActiveRecord::Base
 
   validates :start_date, presence: true
   #validates :end_date
- /   
-  def name(study)
-    if study.ICA?
-      'Informatica'
-    elsif study.IKU?
-      'Informatiekunde'
-    elsif study.GT?
-      'Gametechnologie'
-    elsif study.WISK?
-      'Wiskunde'
-    elsif study.COSC?
-      'Computing science'
-    elsif study.GMTE?
-      'Game and media technology'
-    elsif study.MBIM?
-      'Business informatics'
-    elsif study.AINM?
-      'Artificial intelligence'
-    else
-      'unknown'
-    end
-  end
-  
-  def list
-    [['--', ''],
-    ['INFORMATICA', :ICA], 
-    ['INFORMATIEKUNDE', :IKU], 
-    ['GAMETECHNOLOGIE', :GT], 
-    ['WISKUNDE', :WISK], 
-    ['GAME AND MEDIA TECHNOLOGY', :GMTE], 
-    ['BUSINESS INFORMATICS', :MBIM], 
-    ['ARTIFICIAL INTELLIGENCE', :AINM]]
-  end
-  /
 end
