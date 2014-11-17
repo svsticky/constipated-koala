6d7be6a8cb00b6294538dd6714ca9431e53e0579
Martijn Casteel <martijn.casteel@gmail.com>
2014-11-17 23:31:18 +0100

    Created an overview for the studied that are currently active

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,19 @@
 class HomeController < ApplicationController
   def index
-    #TODO optimize sql query to count
     @members = Education.group('member_id').where('status = 0').length
+    @alumnus = 0
     
     @activities = Activity.count(:all)
     
     #TODO mongoose implementatie
     @sales = 0
+    @credit = 0
     
     #TODO unpayed activities (+ mongoose?)
     @unpayed = Participant.where(:paid => false).sum(:price) + Participant.where(:paid => false, :price => NIL).joins(:activity).where('activities.price IS NOT NULL').sum('activities.price')
+    
+    @studies = Education.joins('study').group('study').count
+    
+    @birthdates = 0
   end
 end
