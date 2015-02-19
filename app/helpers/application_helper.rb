28c0e2fa87f8346dd366953356aeffc2be854fdd
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2015-02-19 14:25:11 +0100

    Refactor navigation
     * This defines two helpers, nav_item and nav_subitem
     * The partials have been moved to the application views folder
     * A route redirect /home -> / has been added to keep the helper API simple
     * The partials are referenced in the correct places

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,38 @@
 module ApplicationHelper
+  # Partial helpers that do the ugly work of defining partial locals.
+  #
+  # Instead of having to write this:
+  #
+  # render partial: 'nav_item', locals: { url: '/', name: 'Home', color:
+  #   'danger', icon: 'desktop'}
+  #
+  # You can write the following:
+  #
+  # nav_item name: 'home', icon: 'desktop', color: 'danger'
+  #
+  #   block_to_partial('shared/sidebar_module'
+
+  # nav_item renders a navigation item
+  #
+  # It takes an options hash with the following items:
+  #   - name:  Name to display in link, should be lowercase
+  #   - icon:  FA icon to use as item background
+  #   - color: CSS class to use for the bg styling
+  #
+  # You can also give it an optional block that you can render subitems in.
+  #
+  # Subitems can be put in the block. These render subitems and take a name and
+  # a url.
+  def nav_item(options = {}, &block)
+    if block_given?
+      options.merge!(:body => capture(&block))
+    end
+    render partial: 'nav_item', locals: options
+  end
+  def nav_subitem(options = {}, &block)
+    if block_given?
+      options.merge!(:body => capture(&block))
+    end
+    render partial: 'nav_subitem', locals: options
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,26 @@
 ConstipatedKoala::Application.routes.draw do
-  
+
   constraints :subdomain => 'intro' do
     scope module: 'users' do
       get  '/', to: 'public#index', as: 'public'
       post '/', to: 'public#create'
-  
+
       get 'confirm', to: 'public#confirm'
     end
   end
 
   constraints :subdomain => 'koala' do
-    
+
     authenticated :user, ->(u) { !u.admin? } do
       root to: 'users/home#index', as: :user_root
     end
-    
+
     root 'admins/home#index'
 
     # No double controllers
     get 'admins/home', to: redirect('/')
     get 'users/home',  to: redirect('/')
+    get 'home',  to: redirect('/')
 
     # Devise routes
     devise_for :users, :skip => [ :registrations ], :path => '', controllers:
@@ -33,8 +34,8 @@ ConstipatedKoala::Application.routes.draw do
     # override route for user profile
     devise_scope :user do
       get   'registration/cancel',  to: 'users/registrations#cancel',   as: :cancel_registration
-      
-      get   'sign_up',              to: 'users/registrations#new',      as: :new_registration      
+
+      get   'sign_up',              to: 'users/registrations#new',      as: :new_registration
       post  'sign_up',              to: 'users/registrations#create',   as: :registration
 
       get   'settings/profile',     to: 'users/registrations#edit',     as: :edit_registration
@@ -45,24 +46,24 @@ ConstipatedKoala::Application.routes.draw do
     scope module: 'admins' do
       # Resource pages
       resources :members, :activities
-  
+
       # Participants routes for JSON calls
       get    'participants/list', to: 'participants#list'
       get    'participants',      to: 'participants#find'
       post   'participants',      to: 'participants#create'
       patch  'participants',      to: 'participants#update'
       delete 'participants',      to: 'participants#destroy'
-      post   'participants/mail', to: 'participants#mail'  
-      
+      post   'participants/mail', to: 'participants#mail'
+
       # search for member using dropdown
       get    'search',                to: 'members#find'
-  
+
       # checkout urls
       get    'checkout',              to: 'checkout#index'
-      
+
       patch  'checkout/card',         to: 'checkout#activate_card'
-      patch  'checkout/transaction',  to: 'checkout#change_funds'  
-      
+      patch  'checkout/transaction',  to: 'checkout#change_funds'
+
       # api routes, own authentication
       get    'checkout/card',         to: 'checkout#information_for_card'
       post   'checkout/card',         to: 'checkout#add_card_to_member'
