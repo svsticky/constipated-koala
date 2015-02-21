48eaefe7cab68191cef5a54ea41c8c30b1f35025
Martijn Casteel <martijn.casteel@gmail.com>
2015-02-21 17:06:10 +0100

    Fixed some issues with the ghostscript using a different colorthingy, thanks to https://gist.github.com/bastien/3059321

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,9 +4,11 @@ class Activity < ActiveRecord::Base
 #  validates :end_date
 #  validates :description
   
-  has_attached_file :poster, :styles => { :thumb => ['180', :png], :medium => ['x720', :png], :original => '100%' }
-  validates_attachment_content_type :poster, :content_type => 'application/pdf'
-#  validates_attachment_size
+  has_attached_file :poster, :styles => { :thumb => ['180', :png], :medium => ['x720', :png] }, 
+	:processors => [:ghostscript, :thumbnail],
+	:convert_options => { :all => '-colorspace CMYK -quality 100 -density 8'}
+  validates_attachment_content_type :poster, :content_type => ['application/pdf', 'image/jpeg', 'image/png']
+#  validates_attachment_size :less_than => 10.megabytes 
 
   has_many :participants, :dependent => :destroy
   has_many :members, :through => :participants
