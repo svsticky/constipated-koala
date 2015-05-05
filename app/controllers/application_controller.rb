cc529948733ece62601b0aa121172ce7868eaf7a
Martijn Casteel <martijn.casteel@gmail.com>
2015-05-05 16:47:37 +0200

    For now a small bugfix such that there are only pdf possible as posters, the ghostscript can't handle anything else than pdf's

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def enable_cors
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
