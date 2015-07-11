1870963cd8a7fddd606b6797f8085cf840cd0487
Martijn Casteel <martijn.casteel@gmail.com>
2015-07-11 12:57:06 +0200

    Fixed the wanbetalers list

--- app/controllers/admins/home_controller.rb
+++ app/controllers/admins/home_controller.rb
@@ -2,15 +2,21 @@ class Admins::HomeController < ApplicationController
   def index
     @members = Education.group('member_id').where('status = 0').length
     @studies = Education.where('status = 0').joins(:study).group('studies.name').order('studies.masters, studies.id').count
-    logger.debug @studies
-    
+
     @activities = Activity.where("start_date >= ?", Date.start_studyyear( Date.current().year )).count
-    
+
     @transactions = CheckoutTransaction.count(:all)
-    
+
     @unpayed = Participant.where(:paid => false).sum(:price) + Participant.where(:paid => false, :price => NIL).joins(:activity).where('activities.price IS NOT NULL').sum('activities.price')
-#    @defaulters = Participant.where(:paid => false).joins(:activity).where('activities.start_date < NOW()').group('member').sum(:price).merge( \
-#      Participant.where(:paid => false, :price => nil).joins(:activity).where('activities.start_date < NOW()') \
-#      .group('member').sum('activities.price') ){ |k, sum_a, sum_b| sum_a + sum_b }.sort_by(&:last).reverse!.take(10)
+
+    @defaulters = Participant.where( :paid => false ).joins( :activity, :member ).where('activities.start_date < NOW()').group( :member_id ).sum( :price ).merge( \
+      Participant.where( :paid => false ).joins( :activity, :member ).where('activities.start_date < NOW()').group( :member_id ).sum( 'activities.price ')
+    ) { |k, sum_a, sum_b| sum_a + sum_b }.sort_by{ |_, sum| -sum }.map{ |k,v| [ Member.where( :id => k).select( :id, :first_name, :infix, :last_name ).first ,v]}.take(15)
+
+    logger.debug @defaulters
+
+    #@defaulters = Participant.where(:paid => false).joins(:activity).where('activities.start_date < NOW()').group('member_id').sum(:price).merge( \
+    #  Participant.where(:paid => false, :price => nil).joins(:activity).where('activities.start_date < NOW()') \
+    #  .group('member').sum('activities.price') ){ |k, sum_a, sum_b| sum_a + sum_b }.sort_by(&:last).reverse!.take(10)
   end
 end
