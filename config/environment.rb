f948ee3e9b445cebd644f08a83a082270b954647
Martijn Casteel <martijn.casteel@gmail.com>
2015-08-20 20:54:40 +0200

    Fixed new error reporting

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
     html_field = Nokogiri::HTML::DocumentFragment.parse(html_tag)
     html_field.children.add_class 'invalid'
 
-    html_field.to_html
+    %{#{html_field.to_html}}.html_safe
   else
     html_tag.html_safe
   end
