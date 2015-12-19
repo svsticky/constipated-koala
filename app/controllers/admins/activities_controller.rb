e7a3512882443bf063f2ad2347534462aa190adb
Martijn Casteel <martijn.casteel@gmail.com>
2015-12-19 15:09:09 +0100

    Even more small fixes; - members css only float to the left when activity list is long enough - non-paid members list updated in e-mail form for members with price is 0

--- app/controllers/admins/activities_controller.rb
+++ app/controllers/admins/activities_controller.rb
@@ -11,7 +11,7 @@ class Admins::ActivitiesController < ApplicationController
 
   def show
     @activity = Activity.find(params[:id])
-    @recipients =  @activity.participants.order('members.first_name', 'members.last_name').joins(:member).where('participants.paid' => false).select(:id, :member_id, :first_name, :email)
+    @recipients =  @activity.participants.order('members.first_name', 'members.last_name').joins(:member).where('participants.paid = FALSE AND (participants.price IS NULL OR participants.price > 0)').select(:id, :member_id, :first_name, :email)
   end
 
   def create
