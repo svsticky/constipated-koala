34725a84e83abdd9ae900897804fe616c946dca2
Martijn Casteel <martijn.casteel@gmail.com>
2015-12-23 19:01:57 +0100

    Remove not implemented tests, well for obvious reasons

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,6 @@
-# encoding: UTF-8
-
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
-
+  # Add more helper methods to be used by all tests here...
 end
