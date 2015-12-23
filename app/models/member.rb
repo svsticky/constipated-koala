bc12710a54c8b64a5e40b5ebe61a7f8a0bdc035a
Martijn Casteel <martijncasteel@users.noreply.github.com>
2015-12-23 18:55:31 +0100

    Added forcing encoding to utf-8 for +- sign studystatus

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # Default a class begins with a number of validations. student_id is special because in the intro website it cannot be empty. However an admin can make it empty
 class Member < ActiveRecord::Base
   validates :first_name, presence: true
