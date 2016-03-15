b1f786f388c9cb57ad8995c90871cf6db7c5a767
Martijn Casteel <martijn.casteel@gmail.com>
2016-03-15 19:48:45 +0100

    Improved ideal views, letting now if dirty-laundromat is down or some error is thrown

--- app/controllers/admins/apps_controller.rb
+++ app/controllers/admins/apps_controller.rb
@@ -55,8 +55,24 @@ class Admins::AppsController < ApplicationController
   end
 
   def ideal
-    @transactions = IdealTransaction.find_by_date( params['date'] || Date.current )
-    @summary = IdealTransaction.summary( @transactions )
+    begin
+      @transactions = IdealTransaction.list( params[:limit] ||= 50, params[:offset] ||= 0 )
+      flash[:warning] = nil
+
+    rescue ArgumentError
+      # redirect or something other than 200
+      flash[:warning] = I18n.t :no_connection, scope: 'activerecord.errors', :name => 'transacties', :url => ENV['IDEAL_PLATFORM']
+      @transactions = IdealTransaction.none
+
+    rescue SocketError
+      # no connection, DNS problems for example, notify user
+      flash[:warning] = I18n.t :no_connection, scope: 'activerecord.errors', :name => 'transacties', :url => ENV['IDEAL_PLATFORM']
+      @transactions = IdealTransaction.none
+
+    end
+
+    @limit = params[:limit] ? params[:limit].to_i : 50
+    @offset = params[:offset] ? params[:offset].to_i : 0
   end
 
   private
