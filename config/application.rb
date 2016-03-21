70e49e6d9614b4d9457a67eefbe91ba54c362180
Martijn Casteel <martijncasteel@users.noreply.github.com>
2016-03-21 18:01:50 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+VERSION = '1.0.3'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
