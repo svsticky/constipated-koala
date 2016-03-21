bdc1359480cbf6e494a8255e1aba43f18133d7d6
Martijn Casteel <martijncasteel@users.noreply.github.com>
2016-03-21 19:21:56 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,14 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-VERSION = '1.0.3'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ConstipatedKoala
   class Application < Rails::Application
+    VERSION = '1.0.3'
+  	
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
