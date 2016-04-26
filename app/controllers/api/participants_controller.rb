77431e5f0d3a8f95b517f53710f0f5111b515495
Martijn Casteel <martijn.casteel@gmail.com>
2016-04-26 19:56:06 +0200

    Improved oauth endpoint

--- app/controllers/api/participants_controller.rb
+++ app/controllers/api/participants_controller.rb
@@ -17,7 +17,7 @@ class Api::ParticipantsController < ApiController
   end
 
   def create
-    @participant = Participant.new( :member =>  Authorization._member, :activity => Activity.find_by_id!(params[:activity_id]))
+    @participant = Participant.new( :member => Authorization._member, :activity => Activity.find_by_id!(params[:activity_id]))
     head :bad_request and return unless @participant.save
 
     render :status => :created and return if params[:bank].blank?
