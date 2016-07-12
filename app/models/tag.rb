88766e0dc672ff1e6cd39ac1a73b420d26b6e969
Martijn Casteel <martijncasteel@users.noreply.github.com>
2016-07-12 13:32:07 +0200

    Update tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,10 +1,10 @@
 class Tag < ActiveRecord::Base
-  enum name: { pardon: 1, merit: 2, honorary: 3 }
+  enum name: { pardon: 1, merit: 2, honorary: 3, donator: 4 }
 
   validates :name, presence: true
   belongs_to :member
   
   def self.active_by_tag
-    [ 1, 2, 3 ]
+    [ 1, 2, 3, 4 ]
   end
 end
