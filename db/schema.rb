6b2b05d5538626845cdf4439dc0d0572fecea432
Yorick van Zweeden <yorici@gmail.com>
2016-08-02 10:05:28 +0200

    Initial work on confirming checkout cards
    - Added migration for table 'checkout_cards'  (Devise's confirmation requires theses attributes)
    - Added confirmable to checkout_card model to mark this class as confirmable for Devise
    - Added separate confirmation method to set the card to active
    - Added required routing (not working properly -> See notes)
    
    Notes:
    - Currently I have used card.active. The identifying key should probably be card.confirmed_at
    - In the model, I had to set the email to the member's email. This can probably be handled in a better way.
    - I was required to add the column 'unconfirmed_email'. This can probably be handled in a better way.
    - The routing does not work correctly. If a path is defined in devise_for ..., devise will look for all of it's files (i.e.: sessions) in that directory. However, when defining no path, the resulting confirmation link will link to /confirmation. Devise will see this a confirmation link for new users.
    
    TL;DR: Devise does not seem to like two confirmation systems side-by-side, or I am just not setting this up correctly. (reverted from commit 6175d23bb6215c327fb29021401af0dba7afe0ff)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801184809) do
+ActiveRecord::Schema.define(version: 20160506121430) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name",                limit: 255
@@ -58,18 +58,13 @@ ActiveRecord::Schema.define(version: 20160801184809) do
   end
 
   create_table "checkout_cards", force: :cascade do |t|
-    t.string   "uuid",                 limit: 16,    null: false
-    t.text     "description",          limit: 65535
+    t.string   "uuid",                limit: 16,    null: false
+    t.text     "description",         limit: 65535
     t.boolean  "active"
-    t.integer  "member_id",            limit: 4,     null: false
-    t.integer  "checkout_balance_id",  limit: 4,     null: false
+    t.integer  "member_id",           limit: 4,     null: false
+    t.integer  "checkout_balance_id", limit: 4,     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "confirmation_token",   limit: 255
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",    limit: 255
-    t.string   "email",                limit: 255
   end
 
   add_index "checkout_cards", ["uuid"], name: "index_checkout_cards_on_uuid", unique: true, using: :btree
