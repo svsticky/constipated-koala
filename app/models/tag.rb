646157a113c16504744e4faad513cd6cba52afb2
M8eld <m.hamers97@gmail.com>
2017-02-28 16:55:37 +0100

    added tag suspended, can't enroll when suspended

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,10 +1,10 @@
 class Tag < ActiveRecord::Base
-  enum name: { pardon: 1, merit: 2, honorary: 3, donator: 4 }
+  enum name: { pardon: 1, merit: 2, honorary: 3, donator: 4, suspended: 5 }
 
   validates :name, presence: true
   belongs_to :member
-  
+
   def self.active_by_tag
-    [ 1, 2, 3, 4 ]
+    [ 1, 2, 3, 4, 5 ]
   end
 end
