024dbe3173b937b65d86d679bbb63c1fd4bee86e
Yorick van Zweeden <yorici@gmail.com>
2017-03-20 21:35:27 +0200

    Fixed payments bug where activities.start_date would be included

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -3,7 +3,7 @@ class Admin::PaymentsController < ApplicationController
   def index
     @detailed = Activity.debtors.sort_by(&:start_date).reverse!
     @last_impressions = Activity.debtors.map { |activity|
-      impression = Impression.where(impressionable_type: Activity).where(impressionable_id: activity.id).where(action_name: "mail").first
+      impression = Impression.where(impressionable_type: Activity).where(impressionable_id: activity.id).where(message: "mail").first
       
       unless impression.nil?
         days = Integer(Date.today - impression.created_at.to_date)
@@ -14,7 +14,7 @@ class Admin::PaymentsController < ApplicationController
     }
 
     # Get members of which the activities have been mailed 4 times, but haven't paid yet
-    @late_activities = Activity.debtors.select { |activity| activity.impressionist_count >= 4 }
+    @late_activities = Activity.debtors.select { |activity| activity.impressionist_count(message: "mail") >= 4 }
     @late_payments = @late_activities.map{ |activity|
       activity.participants.select{ |participant| participant.paid == false }.map{ |p| p.member}
     }.flatten.uniq
