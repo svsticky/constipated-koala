afdc2269c77ad2b0aee9a387b27656fa3f68e5cc
Danny Knemeijer <danny@knemeijer.nl>
2017-03-31 11:36:19 +0200

    Started working on more tests
    @spasticVerbalizer, can you look at why tests can't use controller logic?

--- test/controllers/users/enrollments_controller_test.rb
+++ test/controllers/users/enrollments_controller_test.rb
@@ -1,4 +1,4 @@
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
+class EnrollmentsControllerTest < ActionDispatch::IntegrationTest
   test "enrolling reservist through deleting previous participant" do
     a = activities(:test_enrollments_1)
     assert a.participants.count == 1, "expected one participant on activity, got #{a.participants.count}"
@@ -53,5 +53,21 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert a.participants.count == 2, "expected one participant on activity, got #{a.participants.count}"
     assert a.attendees.count == 2, "expected one attendee on activity, got #{a.attendees.count}"
     assert a.reservists.count == 0, "expected no reservists on activity, got #{a.reservists.count}"
+  end
+
+  test "minors not being able to enroll for 18+ activities" do
+    a = activities(:test_enrollments_3)
+    m1 = members(:m8eld) #underage
+    m2 = members(:thecreator) #adult
+    assert m1.is_underage?, "Expected member being underage, but this member has grown up quite a bit"
+    assert_not m2.is_underage?, "Expected member being 18+, but this member hasn't left puberty yet"
+    p1 = Participant.create!(
+      member: m1,
+      activity: a,
+      reservist: false
+    )
+    a.reload
+    #assert_not p1.save!, "Expected m1 not being able to enroll, but she did"
+
   end
 end
