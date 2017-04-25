0b454dcc21e26ac897769a3dca2f8a6d6a126691
Maarten van den Berg <maartenberg1@gmail.com>
2017-04-25 15:43:41 +0200

    Update default transaction cost

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,7 +1,7 @@
 # year doesn't matter as long as it is in the past, cronjob creates new date incrementing exactly one year
 Settings.defaults[:begin_study_year] = Date.parse( '2009-09-01' )
 
-Settings.defaults[:mongoose_ideal_costs] = 0.45
+Settings.defaults[:mongoose_ideal_costs] = 0.29
 Settings.defaults[:liquor_time] = '16:00'
 
 Settings.defaults[:intro_membership] = [1]
