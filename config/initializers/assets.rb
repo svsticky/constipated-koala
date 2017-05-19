bfd60da604e3415cb22c89ab7ed6803e0ed04e94
Martijn Casteel <martijn.casteel@gmail.com>
2017-05-19 14:25:07 +0200

    Added woff, woff2, and ttf to the asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = ConstipatedKoala::Application::VERSION
 
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
 # Precompile additional assets; application.js, application.css, and all
 # non-JS/CSS in the app/assets folder are already added.
