f3bd9b5e036d9a07cde24a32d821202feea1a844
Tom Wassenberg <tom@tomwassenberg.nl>
2017-05-20 19:06:09 +0200

    Disable serving static assets on production Nginx does this already.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
