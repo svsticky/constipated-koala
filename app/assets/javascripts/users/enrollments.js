040e1dce3c6d973478401cf95f5780fc3fdf392c
Agarwaemben <m.hamers97@gmail.com>
2017-06-06 15:56:38 +0200

    changed to use Math.max

--- app/assets/javascripts/users/enrollments.js
+++ app/assets/javascripts/users/enrollments.js
@@ -179,33 +179,35 @@ $(document).on('ready page:load', function () {
 
 equalheight = function(container){
 
-var currentTallest = 0,
-     currentRowStart = 0,
-     rowDivs = new Array(),
-     $el,
-     topPosition = 0;
- $(container).each(function() {
+  var currentTallest = 0,
+      currentRowStart = 0,
+      rowDivs = new Array(),
+      $el,
+      topPosition = 0;
 
-   $el = $(this);
-   $($el).height('auto')
-   topPostion = $el.position().top;
 
-   if (currentRowStart != topPostion) {
-     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+  $(container).each(function() {
+
+    $el = $(this);
+    $($el).height('auto')
+    topPostion = $el.position().top;
+
+    if (currentRowStart != topPostion) {
+      for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+        rowDivs[currentDiv].height(currentTallest);
+      }
+      rowDivs.length = 0; // empty the array
+      currentRowStart = topPostion;
+      currentTallest = $el.height();
+      rowDivs.push($el);
+    } else {
+      rowDivs.push($el);
+      currentTallest = Math.max(currentTallest, $el.height()); // (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+    }
+    for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
        rowDivs[currentDiv].height(currentTallest);
-     }
-     rowDivs.length = 0; // empty the array
-     currentRowStart = topPostion;
-     currentTallest = $el.height();
-     rowDivs.push($el);
-   } else {
-     rowDivs.push($el);
-     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-  }
-   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-     rowDivs[currentDiv].height(currentTallest);
-   }
- });
+    }
+  });
 }
 
 $(window).load(function() {
