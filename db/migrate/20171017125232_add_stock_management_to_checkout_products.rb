8026830fce8aa350ae9a54bf696fb18971959bbc
Agarwaemben <m.hamers97@gmail.com>
2017-10-17 15:15:08 +0200

    Revert "db migration added for stock"
    This reverts commit 76c52965343276cc1ce905d4d328cca0afa1834b.

--- db/migrate/20171017125232_add_stock_management_to_checkout_products.rb
+++
@@ -1,6 +0,0 @@
-class AddStockManagementToCheckoutProducts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :checkout_products, :storage_stock, :integer, default: 0
-    add_column :checkout_products, :chamber_stock, :integer, default: 0
-  end
-end
