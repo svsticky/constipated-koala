797db8aaca069d94242c3b848211e4e2719fb1f6
Bart Wijgers <bartwijgers@hotmail.com>
2017-11-14 10:42:50 +0100

    Add separate folder for stocky views

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -2,6 +2,6 @@ class Admin::StocksController < ApplicationController
   def index
     @products = CheckoutProduct.order(:category, :name).last_version
 
-    render 'admin/apps/stocky'
+    render 'admin/apps/stocky/voorraad'
   end
 end
