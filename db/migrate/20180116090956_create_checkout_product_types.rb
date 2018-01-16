d159600a50d3a63dadeee89fcfa10b5a0a8929df
Maarten van den Berg <maartenberg1@gmail.com>
2018-01-16 10:48:24 +0100

    Generate CheckoutProductType

---
+++ db/migrate/20180116090956_create_checkout_product_types.rb
@@ -0,0 +1,14 @@
+class CreateCheckoutProductTypes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :checkout_product_types do |t|
+      t.string :name
+      t.integer :category
+      t.boolean :active, default: true
+
+      t.decimal :price, scale: 2, precision: 6
+
+      t.attachment :image
+      t.timestamps
+    end
+  end
+end
