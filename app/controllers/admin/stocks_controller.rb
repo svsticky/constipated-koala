5f1da2560aa8f2278d620a77f3f51a59f5cfe6a4
Marien Raat <marienraat@riseup.net>
2018-01-23 10:56:12 +0100

    Add option to move stocks in stocky

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -1,6 +1,7 @@
 class Admin::StocksController < ApplicationController
   def stock
     @products = CheckoutProduct.order(:category, :name)
+    @stocky_transaction      = StockyTransaction.new
 
     render 'admin/apps/stocky/stock'
   end
@@ -15,6 +16,13 @@ class Admin::StocksController < ApplicationController
   def create
     @stocky_transaction      = StockyTransaction.new(
       stocky_transaction_post_params)
+
+    @stocky_transaction.save
+  end
+
+  def create_purchase
+    @stocky_transaction      = StockyTransaction.new(
+      stocky_purchase_transaction_post_params)
     @stocky_transaction.from = "shop"
     @stocky_transaction.to   = "basement"
 
@@ -26,8 +34,18 @@ class Admin::StocksController < ApplicationController
   end
 
   private
+  def stocky_purchase_transaction_post_params
+    params.require(:stocky_transaction)
+      .permit(:checkout_product_id,
+              :amount)
+  end
+
+  private
   def stocky_transaction_post_params
-    params.require(:stocky_transaction).permit(:checkout_product_id,
-                                               :amount)
+    params.require(:stocky_transaction)
+      .permit(:checkout_product_id,
+              :amount,
+              :from,
+              :to)
   end
 end
