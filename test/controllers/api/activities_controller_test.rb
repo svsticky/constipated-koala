88f6e4574c0303ce1f9fca7f59b4867a188919ed
Maarten van den Berg <maartenberg1@gmail.com>
2018-06-29 17:49:45 +0200

    Update fixtures to be viewable, add Activity + tests

---
+++ test/controllers/api/activities_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class ActivitiesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    host! 'koala.rails.local'
+  end
+
+  test 'should get activities' do
+    get '/api/activities'
+    assert_response :success
+
+    data = JSON.parse @response.body # @response is set automagically on get
+
+    # Assert that we see all activities we should
+    should_see = Activity.where(is_viewable: true).to_a
+    should_see.reject!(&:ended?)
+
+    assert_equal should_see.count, data.count
+
+    seen_names = data.map { |a| a["name"] }
+
+    should_see.each do |a|
+      assert_includes seen_names, a.name
+    end
+  end
+end
