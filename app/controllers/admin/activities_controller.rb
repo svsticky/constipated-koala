cfc41cf5f3507f1b9451d21b4fa05aa7ae5a2ec2
Colin de Roos <colinderoos@hotmail.nl>
2018-09-26 22:18:03 +0200

    fix rubocop
    also, this branch fixes #343

--- app/controllers/admin/activities_controller.rb
+++ app/controllers/admin/activities_controller.rb
@@ -1,6 +1,6 @@
 #:nodoc:
 class Admin::ActivitiesController < ApplicationController
-  impressionist :actions => [ :create, :update, :destroy ]
+  impressionist :actions => [:create, :update, :destroy]
 
   def index
     @activities = Activity.study_year(params['year']).order(start_date: :desc)
