d0595ed9d41f718be91dc050a32bb9fba6e61f51
Maarten van den Berg <maartenberg1@gmail.com>
2018-11-28 12:15:26 +0100

    Fix crash when viewing activities without end_date, re-expose activities from earlier years (#409)
    * Fix crash when viewing activities without end_date
    
    Fixes #403.
    
    * Re-expose year selector in member view
    
    No nice .ui-select because this breaks vertical alignment

--- app/assets/javascripts/members/main.js
+++ app/assets/javascripts/members/main.js
@@ -26,4 +26,11 @@ $(document).on('ready page:load turbolinks:load', function(){
 
       $('#app').children('div').toggleClass('nav-min');
   });
+
+  $('#year').on('change', function(){
+      var params = {};
+
+     params.year = $(this).val();
+     location.search = $.param(params);
+ });
 });
