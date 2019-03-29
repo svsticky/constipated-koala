a8ec49241f6618975e31c85bb53ae8395d968012
Martijn Casteel <martijn.casteel@gmail.com>
2019-03-29 15:03:32 +0100

    Changed member page using a dropdown for updating studystatus

--- app/controllers/members/home_controller.rb
+++ app/controllers/members/home_controller.rb
@@ -61,12 +61,6 @@ class Members::HomeController < MembersController
   def update
     @member = Member.find(current_user.credentials_id)
 
-    if member_post_params['educations_attributes'].to_h.any? { |_id, education| ['active', 'inactive'].include? education['status'] }
-      flash[:error] = I18n.t(:invalid, scope: 'activerecord.errors.models.education.attributes.status')
-      redirect_to users_edit_path
-      return
-    end
-
     if @member.update(member_post_params)
       impressionist(@member, 'lid bewerkt')
 
