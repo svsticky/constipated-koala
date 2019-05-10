fa583c4ce64100425583c1f04a75d9b18a9ba4f3
Colin de Roos <colinderoos@gmail.com>
2019-05-10 12:27:14 +0200

    Add proper destroy handling for add destroy logs

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -5,6 +5,7 @@ class GroupMember < ApplicationRecord
 
   validates :year, presence: true
 
+  before_destroy :rewrite_logs_before_delete!, prepend: true
   is_impressionable dependent: :nullify
 
   def position=(position)
@@ -14,5 +15,24 @@ class GroupMember < ApplicationRecord
 
   def name
     member.name
+  end
+
+  def rewrite_logs_before_delete!
+    impressions.each do |impression|
+      impression.update(message: log_action(impression.action_name))
+    end
+  end
+
+  def log_action(action)
+    I18n.t(
+      action,
+      scope: [:activerecord, :attributes, :impression, :group_member],
+      member_type: (
+        I18n.t group.category, scope: [:activerecord, :attributes, :group, :members]
+      ).to_s.downcase,
+      position: (
+        I18n.t position, scope: [:activerecord, :attributes, :group, :positions], default: position.to_s
+      ).to_s.downcase
+    )
   end
 end
