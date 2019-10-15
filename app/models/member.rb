4379b40f6ec9e701dfd5457d71cad04787135d0a
Maarten van den Berg <maartenberg1@gmail.com>
2019-10-15 17:14:21 +0200

    Member.search: Expose Fuzzily's pagination parameters

--- app/models/member.rb
+++ app/models/member.rb
@@ -177,7 +177,7 @@ class Member < ApplicationRecord
   end
 
   # Functions starting with self are functions on the model not an instance. For example we can now search for members by calling Member.search with a query
-  def self.search(query)
+  def self.search(query, page = 1, limit = 50)
     student_id = query.match(/^\F?\d{6,7}$/i)
     return where("student_id like ?", "%#{ student_id }%") unless student_id.nil?
 
@@ -188,7 +188,7 @@ class Member < ApplicationRecord
     return where(:id => (Education.select(:member_id).where('status = 0').map(&:member_id) + Tag.select(:member_id).where(:name => Tag.active_by_tag).map(&:member_id))) if query.blank?
 
     records = filter(query)
-    return records.find_by_fuzzy_query(query) unless query.blank?
+    return records.find_by_fuzzy_query(query, limit: limit, page: page) unless query.blank?
 
     return records
   end
