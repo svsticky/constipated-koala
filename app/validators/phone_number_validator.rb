8d6508012d24b7e981a669f278a6f6c80ab2b52a
Finn Vos <finnvos34@protonmail.ch>
2019-10-16 13:35:16 +0200

    Add class doc comment

--- app/validators/phone_number_validator.rb
+++ app/validators/phone_number_validator.rb
@@ -1,3 +1,4 @@
+# Checks if a phone number is valid using the telephone_number gem
 class PhoneNumberValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.errors.add(attribute, :invalid) unless TelephoneNumber.parse(value).valid?
