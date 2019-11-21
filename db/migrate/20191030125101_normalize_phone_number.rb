5a2085c86bffdbbc1c6304b8f98f7541e5ed5704
Finn Vos <finnvos34@protonmail.ch>
2019-11-21 15:29:41 +0100

    Match landlines and log invalid numbers

--- db/migrate/20191030125101_normalize_phone_number.rb
+++ db/migrate/20191030125101_normalize_phone_number.rb
@@ -1,11 +1,23 @@
 class NormalizePhoneNumber < ActiveRecord::Migration[5.2]
   def up
     Member.find_each do |m|
-      phone_number = normalize(m.phone_number)
-      emergency_phone_number = normalize(m.emergency_phone_number)
+      # duplicate numbers in case we need the old number
+      phone_number = normalize(m.phone_number.dup)
+      emergency_phone_number = normalize(m.emergency_phone_number.dup)
+
+      comments = m.comments
+
+      if phone_number.nil?
+        comments = log_invalid_number(m.phone_number, comments, false)
+      end
+
+      if emergency_phone_number.nil?
+        comments = log_invalid_number(m.emergency_phone_number, comments, true)
+      end
 
       m.update_attribute :phone_number, phone_number
       m.update_attribute :emergency_phone_number, emergency_phone_number
+      m.update_attribute :comments, comments
     end
   end
 
@@ -16,11 +28,11 @@ class NormalizePhoneNumber < ActiveRecord::Migration[5.2]
   # normalize and validate phone number
   def normalize(number)
     unless number.nil?
-      if number.start_with?("06")
+      if number =~ /^0[^0]/   # match dutch landlines and mobile numbers
         number[0] = "+31"
       end
 
-      if number.start_with?("00")
+      if number.start_with?("00")   # other international numbers
         number[0..1] = "+"
       end
     end
@@ -29,5 +41,16 @@ class NormalizePhoneNumber < ActiveRecord::Migration[5.2]
 
     return number.e164_number if number.valid?
     return nil
+  end
+
+  # add previous number to member comments
+  def log_invalid_number(number, comments, emergency)
+    if comments.nil?
+      comments = ""
+    else
+      comments.concat("\n")
+    end
+
+    comments.concat("Member had invalid #{emergency ? "emergency" : ""} phone number: #{number}")
   end
 end
