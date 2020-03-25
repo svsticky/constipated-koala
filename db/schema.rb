2eb9561debe27c1bcbc4f10ad8a83daa793b94f5
Rick van Hoef <rickvhoef@gmail.com>
2020-03-25 12:11:16 +0100

    added ADS master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_125101) do
+ActiveRecord::Schema.define(version: 2020_03_25_120714) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", null: false
