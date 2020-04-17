5f90fa2104537d050208212806a4610f72f7b627
Rick van Hoef <rickvhoef@gmail.com>
2020-04-17 10:32:06 +0200

    enhanced logging

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -273,6 +273,6 @@ class Activity < ApplicationRecord
 
   # Add a message containing the Activity's id and name to the logs before deleting the activity.
   def rewrite_logs_before_delete
-    impressions.update_all(message: "#{ name } (#{ id })")
+    impressions.update_all(message: "Removed #{ name } (#{ id })")
   end
 end
