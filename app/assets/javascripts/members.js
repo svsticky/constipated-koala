419da59ee31cdff6ab7ac3c5ab56a6d612417fed
Mstiekema <merijnstiekema@outlook.com>
2020-04-27 11:46:57 +0200

    Added language switch logic for javascript

--- app/assets/javascripts/members.js
+++ app/assets/javascripts/members.js
@@ -21,3 +21,14 @@
 //= require toastr
 //
 //= require_tree ./members
+
+// Language logic
+I18n.fallbacks = true;
+I18n.defaultLocale = "nl";
+
+// Language switcher
+const urlParams = new URLSearchParams(window.location.search);
+const language = urlParams.get('l');
+const l = language || sessionStorage.getItem("locale") || I18n.defaultLocale
+sessionStorage.setItem("locale", l);
+I18n.locale = l;
