18b1ca8b1eda5b93e771b30d16b48fb45a5d725f
Finn Vos <f.vos@pm.me>
2020-05-20 16:09:48 +0200

    Check for non-numeric characters in phone number

--- app/validators/phone_number_validator.rb
+++ app/validators/phone_number_validator.rb
@@ -1,6 +1,10 @@
 # Checks if a phone number is valid using the telephone_number gem
+# The telephone_number gem only checks if the string contains a valid phone number,
+# not if the string *only* contains a valid phone number.
+# This is why the second part is needed to compare the string with all
+# non-numberic characters removed to the original string.
 class PhoneNumberValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, :invalid) unless TelephoneNumber.parse(value).valid?
+    record.errors.add(attribute, :invalid) unless TelephoneNumber.parse(value).valid? && value.to_i.to_s == value.gsub("+", "")
   end
 end
