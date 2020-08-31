e3c56b6ad9b50e7bf3c141b572eaef3d1b5ff104
Nireon <siemvdtweel@gmail.com>
2020-08-31 13:12:49 +0200

    Added payment page

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,11 +1,16 @@
 #:nodoc:
 class Payment < ApplicationRecord
   require 'request'
+  include SearchCop
 
   self.primary_key = :token
 
   attr_accessor :issuer, :ideal_uri, :message, :payconiq_qrurl, :payconiq_deeplink
 
+  search_scope :search do
+    attributes :status, :description, :payment_type, :token, :trxid
+    attributes member: %w[member.first_name member.infix member.last_name]
+  end
   validates :description, presence: true
   validates :amount, presence: true, numericality: true
   validates :status, presence: true
@@ -25,8 +30,7 @@ class Payment < ApplicationRecord
 
   after_validation(on: :create) do
     self.amount += transaction_fee
-    puts transaction_fee
-    case :payment_type
+    case payment_type.to_sym
     when :ideal
       http = ConstipatedKoala::Request.new ENV['MOLLIE_DOMAIN']
       self.token = Digest::SHA256.hexdigest("#{ member.id }#{ Time.now.to_f }#{ ideal_redirect_uri }")
@@ -62,8 +66,10 @@ class Payment < ApplicationRecord
       request = http.post("/#{ ENV['PAYCONIQ_VERSION'] }/payments")
 
       request.body = { :amount => (amount * 100).to_i,
+                       :reference => payment_type,
+                       :description => description,
                        :currency => 'EUR',
-                       :callbackUrl => "http://51faf1b25227.ngrok.io/api/hook/payconiq" }.to_json
+                       :callbackUrl => Rails.env.development? ? "#{ ENV['PAYCONIQ_CALLBACKURL'] }/api/hook/payconiq" : Rails.application.url_helpers.payconiq_hook_url }.to_json
 
       request['Authorization'] = "Bearer #{ payconiq_online? ? ENV['PAYCONIQ_ONLINE_TOKEN'] : ENV['PAYCONIQ_DISPLAY_TOKEN'] }"
       request.content_type = 'application/json'
@@ -78,7 +84,7 @@ class Payment < ApplicationRecord
   end
 
   def update!
-    case :payment_type
+    case payment_type.to_sym
     when :ideal
       http = ConstipatedKoala::Request.new ENV['MOLLIE_DOMAIN']
       @status = status
@@ -93,7 +99,7 @@ class Payment < ApplicationRecord
       # first time paid as a response
       return true if status == 'paid' && @status != 'paid'
 
-      message = I18n.t('processed', scope: 'activerecord.errors.models.ideal_transaction')
+      self.message = I18n.t('processed', scope: 'activerecord.errors.models.ideal_transaction')
       return false
     when :pin
     else
@@ -120,7 +126,6 @@ class Payment < ApplicationRecord
 
   # mark transaction_types as paid
   def finalize!
-    puts transaction_type
     case transaction_type.to_sym
     when :activity
 
@@ -133,13 +138,12 @@ class Payment < ApplicationRecord
       self.message = I18n.t('success', scope: 'activerecord.errors.models.ideal_transaction')
 
     when :checkout
-      puts transaction_id
       # additional check if not already added checkout funds
       return unless transaction_id.empty?
 
       # create a single transaction to update the checkoutbalance and mark the Payment as processed
       Payment.transaction do
-        transaction = CheckoutTransaction.create!(:price => (amount - transaction_fee), :checkout_balance => CheckoutBalance.find_by_member_id!(member), :payment_method => :payment_type)
+        transaction = CheckoutTransaction.create!(:price => (amount - transaction_fee), :checkout_balance => CheckoutBalance.find_by_member_id!(member), :payment_method => payment_type)
 
         self.transaction_id = [transaction.id]
         save!
@@ -180,6 +184,6 @@ class Payment < ApplicationRecord
   end
 
   def activities
-    Activity.find(transaction_id) if :activity?
+    Activity.find(transaction_id) if activity?
   end
 end
