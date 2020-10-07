727b608ae1276cd59b27b76ddfb8d1056f1064fa
Mstiekema <merijnstiekema@outlook.com>
2020-10-07 10:37:07 +0200

    Added translations

--- app/models/post.rb
+++ app/models/post.rb
@@ -43,13 +43,3 @@ class Post < ApplicationRecord
     Settings['posts.pinned'] = Settings['posts.pinned'].reject { |i| id == i }
   end
 end
-
-# t.text "content"
-# t.integer "status", default: 0, null: false
-# t.string "tags"
-# t.string "author_type", null: false
-# t.bigint "author_id", null: false
-# t.datetime "created_at", precision: 6, null: false
-# t.datetime "updated_at", precision: 6, null: false
-# t.datetime "published_at"
-# t.index ["author_type", "author_id"], name: "index_posts_on_author_type_and_author_id"
