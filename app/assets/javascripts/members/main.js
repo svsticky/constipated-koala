72c96a9fe725a582542473fbe9d1255a9160af1e
Finn Vos <finn@fvos.de>
2020-10-08 12:36:56 +0200

    Fix participant table update after enrolling

--- app/assets/javascripts/members/main.js
+++ app/assets/javascripts/members/main.js
@@ -1,6 +1,14 @@
 $(document).on("ready page:load turbolinks:load", function () {
   setup_intl_tel_input();
 
+  // Needed to update the participant and reservist tables in the activity view
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+  };
+
   // Callback handler for menu
   $(".toggle-min").click(function (event) {
     event.preventDefault();
