2dae65eba5b6053f9b6532ef115707df382f5749
Mstiekema <merijnstiekema@outlook.com>
2020-10-17 20:27:21 +0200

    Using sidekiq to schedule posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,5 @@
 #:nodoc:
-class Admin::PostsController < ApplicationController
+class Admin::PostsController < ApplicationController  
   def index
     # TODO: add filters, for pinned draft etc
     @pagination, @posts = pagy(Post.all)
@@ -28,6 +28,11 @@ class Admin::PostsController < ApplicationController
     @post = Post.find_by_id params[:id]
     
     if @post.update(post_params)
+      if post_params["status"] == "scheduled"
+        @time_until_post = DateTime.parse(post_params[:published_at])
+        Post.perform_at(@time_until_post, params[:id])
+      end
+
       redirect_to @post
     else
       @pagination, @posts = pagy(Post.all)
--- lib/tasks/posts.rake
+++
@@ -1,6 +0,0 @@
-namespace :posts do
-  desc 'Publish a post whenever it\'s scheduled to release'
-  task :publish => :environment do
-    Post.where(status: :scheduled).where('published_at <= ?', Time.current).update_all(status: :published)
-  end
-end
