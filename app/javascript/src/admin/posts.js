ec66039328547ee076414e0910162d0172f59d53
Mstiekema <merijnstiekema@outlook.com>
2020-10-18 14:03:19 +0200

    Added new datepicker

---
+++ app/javascript/src/admin/posts.js
@@ -0,0 +1,8 @@
+import "flatpickr"
+
+$(document).on("ready page:load turbolinks:load", function () {
+  $(".publish-datetime")[0].flatpickr({
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+  });
+});
