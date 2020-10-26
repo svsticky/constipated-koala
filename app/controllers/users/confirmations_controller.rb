0b4b20074443a6e67c55d2b32c2a953a0f0fa7d2
Nireon <siemvdtweel@gmail.com>
2020-10-26 10:52:51 +0100

    Update confirmations_controller.rb

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -6,8 +6,10 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   def show
     # change email for member first using unconfirmed_email, then super, super ends the function
     user = User.find_by_confirmation_token params['confirmation_token']
-    user.credentials.update_column(:email, user.unconfirmed_email) unless user.nil? || user.unconfirmed_email.nil? || user.admin?
-    user.credentials.update_fuzzy_query!
+    if (!(user.nil? || user.uncofirmed_email.nil? || user.admin?))
+      user.credentials.update_column(:email, user.unconfirmed_email)
+      user.credentials.update_fuzzy_query!
+    end
 
     super
   end
