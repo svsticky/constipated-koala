1b82a3f133fa90fe53a7478971b41e82d3a5f72b
Rick van Hoef <rickvhoef@gmail.com>
2020-11-12 22:28:55 +0100

    install node modules with nix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = ConstipatedKoala::Application::VERSION
 
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << ENV['NODE_PATH'].split(':')[0]
 
 # added view paths for rabl
 Rabl.configure do |config|
