6c2ec4c5d18c8740f09f992a936c339487c52081
Rick van Hoef <rickvhoef@gmail.com>
2020-12-03 10:55:25 +0100

    fix cfrs / rails-ujs

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,18 @@
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
+const webpack = require('webpack')
 
 environment.loaders.prepend('erb', erb)
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default'],
+    toastr: 'toastr/toastr',
+    ApexCharts: ['apexcharts', 'default'],
+    underscore: ['underscore', 'm'],
+    Rails: ['@rails/ujs']
+  })
+)
 module.exports = environment
