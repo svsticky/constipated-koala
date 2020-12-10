4ae0887de4c0977db95a34f98afc6c9bb44faa9f
Finn Vos <finn@fvos.de>
2020-12-10 17:43:26 +0100

    Update email address in mailchimp when a member confirms an email change

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -5,8 +5,9 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # This method is used to confirm account on creation as well as confirmation on updating email
   def show
     # change email for member first using unconfirmed_email, then super, super ends the function
-    user = User.find_by_confirmation_token params['confirmation_token']
-    unless user.nil? || user.uncofirmed_email.nil? || user.admin?
+    user = User.find_by_confirmation_token params['confirmation_token'] unless params['confirmation_token'].nil?
+    unless user.nil? || user.unconfirmed_email.nil? || user.admin?
+      MailchimpUpdateMailJob.perform_later(user.email, user.unconfirmed_email)
       user.credentials.update_column(:email, user.unconfirmed_email)
       user.credentials.update_fuzzy_query!
     end
