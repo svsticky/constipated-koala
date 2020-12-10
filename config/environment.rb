3da1998232fef4e2e5bf6babd3447d088ae4778d
Rick van Hoef <rickvhoef@gmail.com>
2020-12-10 20:21:13 +0100

    set log directory

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
 
+if ENV['LOG_DIRECTORY'].present?
+    Rails.logger = Logger.new File.open(ENV['LOG_DIRECTORY'], 'a')
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
