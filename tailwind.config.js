8524ca4c421b359474653c35c21b7a064d5d467a
Rick van Hoef <rickvhoef@gmail.com>
2021-07-07 22:05:18 +0200

    use colors that conflict less with bootstrap

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,10 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
-    colors: {
-      // these colors are copied from Yeti
-      board: 'fe7215',
-      transparent: 'transparent',
-      blue: '#008cba',
-      indigo: '#6610f2',
-      purple: '#6f42c1',
-      pink: '#e83e8c',
-      red: '#F04124',
-      orange: '#fd7e14',
-      yellow: '#E99002',
-      green: '#43ac6a',
-      teal: '#20c997',
-      cyan: '#5bc0de',
-      white: '#fff',
-      gray: '#888',
-      'gray-dark': '#333',
-      primary: '#008cba',
-      secondary: '#eee',
-      success: '#43ac6a',
-      info: '#5bc0d',
-      warning: '#E99002',
-      danger: '#F04124',
-      light: '#eee',
-      dark: '#222',
+    extend: {
+      colors: {
+        board: 'fe7215',
+      }
     },
     screens: {
       // these breakpoints are copied from Yeti
