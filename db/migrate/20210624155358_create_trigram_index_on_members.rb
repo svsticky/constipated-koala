942d9e435232e18496ac7654d5659f69cab0d1ad
Rick van Hoef <rickvhoef@gmail.com>
2021-07-29 11:18:56 +0200

    don't trigram infices

--- db/migrate/20210624155358_create_trigram_index_on_members.rb
+++ db/migrate/20210624155358_create_trigram_index_on_members.rb
@@ -3,9 +3,9 @@ class CreateTrigramIndexOnMembers < ActiveRecord::Migration[6.0]
   self.disable_ddl_transaction!
 
   def up
-    # Don't try to merge these statements, a transaction will be created (wtf rails)
+    # Don't try to merge these statements, a transaction will be created and the statements will fail.
+    # This behaviour is undocumented.
     execute "CREATE INDEX CONCURRENTLY member_first_names ON members USING GIN(first_name gin_trgm_ops);"
-    execute "CREATE INDEX CONCURRENTLY member_infices ON members USING GIN(infix gin_trgm_ops);"
     execute "CREATE INDEX CONCURRENTLY member_last_names ON members USING GIN(last_name gin_trgm_ops);"
     execute "CREATE INDEX CONCURRENTLY member_email ON members USING GIN(phone_number gin_trgm_ops);"
     execute "CREATE INDEX CONCURRENTLY member_phone_number ON members USING GIN(phone_number gin_trgm_ops);"
@@ -14,7 +14,6 @@ class CreateTrigramIndexOnMembers < ActiveRecord::Migration[6.0]
 
   def down
     execute "DROP INDEX member_first_names;"
-    execute "DROP INDEX member_infices;"
     execute "DROP INDEX member_last_names;"
     execute "DROP INDEX member_email;"
     execute "DROP INDEX member_phone_number;"
