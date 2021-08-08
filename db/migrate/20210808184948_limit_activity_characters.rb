a644d83fc8bf8d6d756ddc04c81f34c5c48aebc6
justnireon <siemvdtweel@gmail.com>
2021-08-08 21:10:04 +0200

    Fix char limit activities

---
+++ db/migrate/20210808184948_limit_activity_characters.rb
@@ -0,0 +1,11 @@
+class LimitActivityCharacters < ActiveRecord::Migration[6.0]
+  def up
+    Activity.where('length(name) > 52').each {|activity|
+    activity.comments = activity.notes + "\n Activity name was too long\n Original: #{activity.name}"
+    activity.name = activity.name[0..51]
+    activity.save
+  }
+  
+    change_column :activities, :name, :string, :limit => 52
+  end
+end
