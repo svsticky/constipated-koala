9fc3c50d0b512524c29107b48222576481f69fc6
Jasper Albering <jasper@albering.nl>
2021-11-08 18:08:15 +0100

    Add tailwind jit purge compilation
    JIT mode is still in preview but speeds up build.
    This is not compatible with semantic versioning though.
    CSS bundle size is reduced 20x as purge was not working correctly before.

--- postcss.config.js
+++ postcss.config.js
@@ -10,14 +10,5 @@ module.exports = {
       },
       stage: 3
     }),
-    // don't purge css in development
-    process.env.NODE_ENV === 'development' ? null :
-    require("@fullhuman/postcss-purgecss")({
-      content: [
-        "./app/**/*.haml",
-        "./app/javascript/**/*.js"
-      ],
-      defaultExtractor: content => content.match(/[^<>"{\.'`\s]*[^<>"{\.'`\s:]/g) || []
-    })
   ]
 }
