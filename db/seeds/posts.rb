e488b5f11a8e9bbe2d8caa93d608748b803f8a90
Mstiekema <github@mstiekema.nl>
2022-04-05 09:49:24 +0200

    Fixed packages & seed, removed unused code

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -6,24 +6,16 @@ puts '-- Creating posts'
   created_at = Faker::Time.between(from: 2.years.ago, to: Date.yesterday)
 
   Post.create(
-    title: "Bestuurs bericht",
+    title: Faker::Movie.title,
     status: [0,1].sample,
-    tags: 'test test2',
+    tags: Faker::Lorem.sentence(word_count: 2),
 
     author: Admin.all.sample,
 
     created_at: created_at,
     published_at: Faker::Time.between(from: created_at, to: Date.today),
 
-    content: <<~EOT
-      <h5>Maandag 13:30: Boulderen / Bouldering</h5>
-      WEL klimmen, GEEN zekering nodig. Kom vrij klimmen in Boulderhal Energiehaven met de sportcommissie!
-      <a href="https://koala.svsticky.nl/activities/909">https://koala.svsticky.nl/activities/909</a>
-
-      <h5>Dinsdag 13:00: Ode to your CODE (hulpmiddag)</h5>
-      Op 21 januari, een week voor het grote CODE practicum, organiseert Sticky een hulpmiddag in BBG 1.79 van 13:00u tot 17:00u. Hier kan je extra vragen stellen en de laatste hand leggen aan je practicum. Wij zorgen voor koffie, thee en koekjes.
-      <a href="https://koala.svsticky.nl/activities/904">https://koala.svsticky.nl/activities/904</a>
-    EOT
+    content: Faker::Lorem.paragraph(sentence_count: 5)
   )
 
 end
