ef450673ad762c2e3508b2d4c8ecbd56bfa76fa7
Rick van Hoef <rickvhoef@gmail.com>
2022-04-21 20:59:59 +0200

    fix rubocop and add comments

--- app/mailers/mailings/payment.rb
+++ app/mailers/mailings/payment.rb
@@ -1,46 +1,51 @@
+#:nodoc:
 module Mailings
-  class Payment < ApplicationMailer
+  # The requestmail sends a payment request to the passed member. This payment
+  # mail lists all open payments and the total amount that should be paid.
+  # Only pass members that actually have open payments, otherwise the email will
+  # ask for a payment of 0 euro.
+  class PaymentMailer < ApplicationMailer
     def requestmail(member)
-      # return if ENV['MAILGUN_TOKEN'].blank? # TODO is this a good plan?
+      # Send the email in the members' prefered language
       I18n.locale = member.language
-
-      email = member.email
-      first_name = member.first_name,
 
       subject = I18n.t('mailings.payment.subject')
 
+      # calculate the total amount and build a string containing the activities.
       total = 0
-
       activities = ""
       member.unpaid_activities.each do |activity|
         participant = member.participant_by_activity(activity)
-        activities += "#{activity.name} (#{ActionController::Base.helpers.number_to_currency(participant.currency, unit: '€')})"
+        activities += "#{ activity.name } (#{
+          ActionController::Base.helpers.number_to_currency(
+            participant.currency,
+            unit: '€'
+          ) })"
         total += activity.price
       end
 
-      puts member
-
+      # Render the html version of the email
       html = render_to_string(locals: {
-        name: member.first_name,
-        activities: activities,
-        total: total,
-      })
+                                name: member.first_name,
+                                activities: activities,
+                                total: total
+                              })
 
+      # Render the plaintext version of the email
       text = <<~TEXT
-        #{I18n.t('mailings.greeting')} #{member.first_name}
-    
-        #{I18n.t('mailings.payment.open_activities')}
-        #{activities}
-    
-        #{I18n.t('mailings.payment.total_of', amount: total)}
-    
-        #{I18n.t('mailings.best_regards')}
-    
-        #{I18n.t('mailings.signature')}
+        #{ I18n.t('mailings.greeting') } #{ member.first_name }
+
+        #{ I18n.t('mailings.payment.open_activities') }
+        #{ activities }
+
+        #{ I18n.t('mailings.payment.total_of', amount: total) }
+
+        #{ I18n.t('mailings.best_regards') }
+
+        #{ I18n.t('mailings.signature') }
       TEXT
 
       return mail(member.email, nil, subject, html, text)
     end
   end
 end
-  
