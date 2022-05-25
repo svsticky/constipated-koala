422ff263c93479d59ccedb02904946e69768dd62
Siem van den Tweel <siemvdtweel@gmail.com>
2022-05-25 10:22:48 +0200

    fixes

--- db/migrate/20220524203723_add_is_payable_updated_at.rb
+++ db/migrate/20220524203723_add_is_payable_updated_at.rb
@@ -1,5 +1,6 @@
 class AddIsPayableUpdatedAt < ActiveRecord::Migration[6.1]
   def change
     add_column :activities, :is_payable_updated_at, :timestamptz
+    Activity.where(is_payable:true).update_all(is_payable_updated_at: DateTime.now-3.weeks)
   end
 end
