c4b56618da4ac5bec5d51d5c1f99f8174dfc06e6
Maarten van den Berg <maartenberg1+git@gmail.com>
2023-10-31 21:02:04 +0100

    Bump version to v2.20.5

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module ConstipatedKoala
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    VERSION = '2.20.3'.freeze
+    VERSION = '2.20.5'.freeze
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
