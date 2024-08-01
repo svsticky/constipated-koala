edb4402e9779df01cd8c59caf1a204adab564f38
Mervin de Jong <mervin.dj@hotmail.com>
2024-08-01 15:53:16 +0200

    Fixed bugs in form validation on the intro page

--- app/javascript/src/language.js
+++ app/javascript/src/language.js
@@ -1,4 +1,4 @@
-import I18n from "i18n-js";
+import I18n from "./translations";
 import Cookies from "js-cookie";
 
 // Language logic
@@ -6,7 +6,9 @@ I18n.fallbacks = true;
 I18n.defaultLocale = "nl";
 
 // Language switcher
-var urlParams = new URLSearchParams(window.location.search);
-var urlLocale = urlParams.get("l");
+const urlParams = new URLSearchParams(window.location.search);
+const urlLocale = urlParams.get("l");
 
-I18n.locale = Cookies.get("locale") || urlLocale || I18n.defaultLocale;
+I18n.locale = urlLocale || Cookies.get("locale") || I18n.defaultLocale;
+
+export default I18n;
