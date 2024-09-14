c995266ac51e769a8ab16f3b42cb82e59542aa20
Mervin de Jong <mervin.dj@hotmail.com>
2024-09-14 19:04:29 +0200

    Added verbose (failure) logging to the seeding process

--- db/seeds/members.rb
+++ db/seeds/members.rb
@@ -2,26 +2,29 @@
 puts '-- Creating members'
 60.times do
   first_name = Faker::Name.first_name
-  last_name  = Faker::Name.last_name
+  last_name = Faker::Name.last_name
 
   member = Member.create(
-    first_name:   first_name,
-    infix:        (Faker::Number.within(range: 1..10) > 7 ? Faker::Name.tussenvoegsel : ' '),
-    last_name:    last_name,
-    address:      Faker::Address.street_name,
+    first_name: first_name,
+    infix: (Faker::Number.within(range: 1..10) > 7 ? Faker::Name.tussenvoegsel : ' '),
+    last_name: last_name,
+    address: Faker::Address.street_name,
     house_number: Faker::Address.building_number,
-    postal_code:  Faker::Address.postcode,
-    city:         Faker::Address.city,
+    postal_code: Faker::Address.postcode,
+    city: Faker::Address.city,
     phone_number: Faker::Base.numerify('+316########'),
     emergency_phone_number: Faker::Base.numerify('+316########'),
-    email:        Faker::Internet.safe_email(name: (first_name + '.' + last_name).parameterize),
-    student_id:   "F#{ Faker::Number.number(digits: 6) }",
-    birth_date:   Faker::Date.between(from: 28.years.ago, to: 16.years.ago),
-    join_date:    Faker::Date.between(from: 6.years.ago, to: Date.today),
-    comments:     (Faker::Boolean.boolean(true_ratio: 0.3) ? Faker::Hacker.say_something_smart : nil)
+    email: Faker::Internet.safe_email(name: (first_name + '.' + last_name).parameterize),
+    student_id: "F#{ Faker::Number.number(digits: 6) }",
+    birth_date: Faker::Date.between(from: 28.years.ago, to: 16.years.ago),
+    join_date: Faker::Date.between(from: 6.years.ago, to: Date.today),
+    comments: (Faker::Boolean.boolean(true_ratio: 0.3) ? Faker::Hacker.say_something_smart : nil)
   )
 
-  puts("   -> #{ member.name } (#{ member.student_id })")
+  puts "   [#{ member.valid? ? ' Ok ' : 'Fail' }] #{ member.name } (#{ member.student_id })"
+  member.errors.objects.each do |error|
+    puts "          > #{error.full_message}"
+  end
 end
 
 puts '-- Creating educations'
@@ -29,17 +32,22 @@ Member.all.each do |member|
   Faker::Number.within(range: 1..3).times do
     # because of the [member, study, start_date] key this goes wrong often
     suppress(ActiveRecord::RecordNotUnique) do
-      status     = Faker::Number.within(range: 0..2)
+      status = Faker::Number.within(range: 0..2)
       start_date = Faker::Date.between(from: member.join_date, to: Date.today)
-      end_date   = (status > 0 ? Faker::Date.between(from: start_date, to: Date.today) : nil)
+      end_date = (status > 0 ? Faker::Date.between(from: start_date, to: Date.today) : nil)
 
-      Education.create(
-        member_id:  member.id,
-        study_id:   Study.all.sample.id,
+      education = Education.create(
+        member_id: member.id,
+        study_id: Study.all.sample.id,
         start_date: start_date,
-        end_date:   end_date,
-        status:     status
+        end_date: end_date,
+        status: status
       )
+
+      puts "   [#{ education.valid? ? ' Ok ' : 'Fail' }] #{ education.study_id } for #{ education.member_id }"
+      education.errors.objects.each do |error|
+        puts "          > #{error.full_message}"
+      end
     end
   end
 end
--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -2,34 +2,46 @@ require Rails.root.join('db', 'seeds', 'members.rb')
 
 # Creating login accounts
 puts '-- Creating users'
-Admin.create(
-  email:    'dev@svsticky.nl',
+
+dev = Admin.create(
+  email: 'dev@svsticky.nl',
   password: 'sticky123'
 )
-puts '   -> dev@sticky.nl (admin)'
 
-Admin.create(
-  email:      'martijn@svsticky.nl',
-  password:   'sticky123',
+puts "   [#{ dev.valid? ? ' Ok ' : 'Fail' }] dev@sticky.nl (admin)"
+dev.errors.objects.each do |error|
+  puts "          > #{error.full_message}"
+end
+
+martijn = Admin.create(
+  email: 'martijn@svsticky.nl',
+  password: 'sticky123',
 
   first_name: 'Martijn',
-  last_name:  'Casteel',
-  signature:  'Martijn'
+  last_name: 'Casteel',
+  signature: 'Martijn'
 )
-puts '   -> martijn@sticky.nl (admin)'
+
+puts "   [#{ martijn.valid? ? ' Ok ' : 'Fail' }] martijn@sticky.nl (admin)"
+martijn.errors.objects.each do |error|
+  puts "          > #{error.full_message}"
+end
 
 # create actual members, but give the first member test@svsticky.nl
 member = Member.joins(:educations).where(educations: { status: 0 }).first
 member.update(email: 'test@svsticky.nl')
 
 user = User.new(
-  email:       member.email,
-  password:    'sticky123',
+  email: member.email,
+  password: 'sticky123',
   credentials: member,
-  language:    Faker::Number.within(range: 0..1)
+  language: Faker::Number.within(range: 0..1)
 )
 
 user.skip_confirmation!
 user.save!
 
-puts "   -> #{ user.email } (member)"
+puts "   [#{ user.valid? ? ' Ok ' : 'Fail' }] #{ user.email } (member)"
+user.errors.objects.each do |error|
+  puts "          > #{error.full_message}"
+end
