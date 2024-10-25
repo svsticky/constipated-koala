01c3d07eb13ae40fbb4aae4845b5e096810364a4
Silas Peters <69711739+SilasPeters@users.noreply.github.com>
2024-10-25 15:55:48 +0200

    feat: personalised ical feed (#1101)
    * feat: calendar endpoint
    
    * feat: added button to copy personalised webcal link
    
    * feat: if participant is enrolled as reservist, activity is marked as such
    
    * chore: expanded database, processed TODOs and handled exceptiosn
    
    * chore: conform to linter
    
    * fix: expand test fixture
    
    * fix: whole-day activities are now properly formatted
    
    * fix: properly respond to HEAD request
    
    * refactor: move activity_to_event to activity model
    
    * chore: localised everything
    
    * chore: conform to linters
    
    * fix: normalised locales
    
    * feat: adds clarification on icalendar feed
    
    * feat: added disclaimer in activity template
    
    * fixup! feat: added disclaimer in activity template
    
    * fixup! fixup! feat: added disclaimer in activity template
    
    * fixup! Merge branch 'master' into feat/personalised-ical-feed
    
    * fixup! Merge branch 'master' into feat/personalised-ical-feed
    
    * fix: locale translation

---
+++ lib/icalendar_helper.rb
@@ -0,0 +1,22 @@
+require 'icalendar' # https://github.com/icalendar/icalendar
+
+# Includes all abstractions over iCalender representations
+module IcalendarHelper
+  # Combines zero or more Icalendar events into an iCalendar abstract object
+  def self.create_calendar(events, locale)
+    calendar = Icalendar::Calendar.new
+    calendar.x_wr_calname = I18n.t("calendars.personalised_activities_calendar.name", locale: locale)
+    events.each { |e| calendar.add_event(e) }
+    calendar.publish
+    return calendar
+    # Returns the abstract icalendar object, not the ICS string ready to
+    # be stored in an ICS file. To convert this calendar into an ICS string,
+    # use `calendar.to_ical` or the `create_file` method below.
+  end
+
+  # Stores the calendar to an *.ics file
+  def self.create_file(calendar, path)
+    calendar_string = calendar.to_ical
+    File.write(path, calendar_string)
+  end
+end
