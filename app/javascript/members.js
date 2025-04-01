77e6446259e38f09aca1361ceef5685adfe39435
sam32123 <samstaijen@gmail.com>
2025-04-01 19:22:41 +0200

    revert addidition of clippy

--- app/javascript/members.js
+++ app/javascript/members.js
@@ -10,3 +10,11 @@ import "./src/members/activities/activity";
 import "./src/members/activities/poster_modal";
 import "./src/members/payments";
 import "./src/language";
+
+const flag = btoa("ctf_28168543");
+
+console.error(`ctf = ${flag}`);
+
+window.setTimeout(async () => {
+  await fetch("https://hacc.svsticky.nl/ctf_88299184");
+}, 60 * 1000);
