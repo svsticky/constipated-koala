1a0a4504617f341b619fd98fb93a3ee1f95a0b7b
Job Vonk <sticcpistt@icloud.com>
2025-12-09 19:39:02 +0100

    feat: add from and to parameters to activities api endpoint (#1208)
    * feat: add from and to parameters to activities api endpoint
    
    * Fix linter offenses
    
    * Fixed bug with ended activities not showing up
    
    * Added missing @

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -12,12 +12,19 @@ class Api::ActivitiesController < ApiController
         params[:date],
         params[:date]
       ).order(:start_date).limit(params[:limit] ||= 10).offset(params[:offset] ||= 0)
-
     else
-      @activities = Activity.where('(end_date IS NULL AND start_date >= ?) OR end_date >= ?',
-                                   Date.today, Date.today).order(:start_date).where(is_viewable: true)
-      @activities.limit!(params[:limit]).offset(params[:offset] ||= 0) if params[:limit].present?
-      @activities = @activities.reject(&:ended?)
+      from = params[:from].present? ? Date.parse(params[:from]) : Date.today
+      @activities = Activity.where('start_date >= ?', from)
+                            .order(:start_date)
+                            .where(is_viewable: true)
+
+      # Allow nil to mean no limit on the end date
+      @activities = @activities.where('end_date <= ?', Date.parse(params[:to])) if params[:to].present?
+
+      @activities = @activities.limit!(params[:limit]).offset(params[:offset] ||= 0) if params[:limit].present?
+
+      @activities = @activities.reject(&:ended?) if params[:from].blank?
+      @activities
     end
   end
 
